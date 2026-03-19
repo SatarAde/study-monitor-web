3acf319a9c3f8831e31ee5ba3758d4a2e00e2157
SatarAde <sataradebayo1@gmail.com>
2026-03-19 15:31:23 +0900

    Add Firebase config

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,11 +1,12 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app';
+import {
+  getFirestore, collection, addDoc, onSnapshot,
+  orderBy, query, serverTimestamp, doc, updateDoc, setDoc
+} from 'firebase/firestore';
+import {
+  getStorage, ref, uploadBytes, getDownloadURL
+} from 'firebase/storage';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDMEsEgUKMVNHrHNKsd5ZwVgavl-YEqLRg",
   authDomain: "study-monitor-5febe.firebaseapp.com",
@@ -16,6 +17,6 @@ const firebaseConfig = {
   measurementId: "G-RLDQVX5VJ4"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
